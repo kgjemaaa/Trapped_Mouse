2701cce8dbb3d706050b561915cbfa2cbe186b48
kgjemaaa <53934098+kgjemaaa@users.noreply.github.com>
2020-01-13 00:37:04 +0100

    Update TrappedMouse_Controller.java

--- TrappedMouse_Controller.java
+++ TrappedMouse_Controller.java
@@ -1,10 +1,10 @@
 public class TrappedMouse_Controller {
    public static void main(String[] args) {
-   
+   //Thirrja e klasave TrappedMouse_Model dhe TrapepdMouse_View
       TrappedMouse_Model model = new TrappedMouse_Model();
       TrappedMouse_View view = new TrappedMouse_View(model.get_mouse());
       
-      
+      //kommunikimi me shfrytezuesin
       view.add_listener(new java.awt.event.WindowAdapter() {
           @Override
           public void windowClosing(java.awt.event.WindowEvent windowEvent) {
@@ -20,17 +20,20 @@ public class TrappedMouse_Controller {
               }
           }
       });
-      
-      
+      //pozicioni fillestar i miut
       model.set_x_position(20);
       model.set_y_position(20);
+      //pozicioni i tanishem i miut
       view.set_x_position(model.get_x_position());
       view.set_y_position(model.get_y_position());
+      //metodat per ndrrimin e drejtimit te miut
       view.set_mouse_left(model.get_mouse_left());
       view.set_mouse_right(model.get_mouse_right());
       view.set_mouse_up(model.get_mouse_up());
       view.set_mouse_down(model.get_mouse_down());
+      //metoda qe e merr urdherin nga perdoruesi, mund te gjendet te klasa TrapepdMouse_View 
       view.get_direction();
+      //metoda per vizatimin e miut
       model.set_mouse(view.get_mouse());
    }
 }
